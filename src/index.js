349c62738ead03e2a8f39dcb77aa14c91dc27955
nbvishal@noborderz.com <vishal.thoughtwin@gmail.com>
2022-06-14 13:29:47 +0530

    folder Stracture change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CountNumberReducer from './UseReducer/CountNumberReducer';
+import CountNumberReducer from './UseReducerHook/CountNumberReducer';
 // import CompParent from './useContext/rcc/CompParent';
 // import App from './useContext/rfc/App'
-// import UseReducer from './UseReducer/UseReducer';
-// import UseRefRcc from './useref/rcc/UseRefRcc';
-// import {UseRefHook} from './useref/rfc/UseRefHook';
+// import UseReducer from './UseReducerHook/UseReducer';
+// import UseRefRcc from './useRefHook/rcc/UseRefRcc';
+// import {UseRefHook} from './useRefHook/rfc/UseRefHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
